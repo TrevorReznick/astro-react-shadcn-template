58d90c6c3cf9c6c5086e148ac6345893926b49be
joseph.k <double.facessss@gmail.com>
2025-03-16 21:07:29 +0100

    OPF-56 fixing dev-deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,13 @@ import react from '@astrojs/react'
 
 import tailwind from '@astrojs/tailwind'
 
+import sentry from '@sentry/astro';
+import spotlightjs from '@spotlightjs/astro';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false,
+  }), sentry(), spotlightjs()],
 })
