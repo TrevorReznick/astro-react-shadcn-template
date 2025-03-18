0b96361f22fc47e961b34c9497a1f2f33c3b7a9b
joseph.k <double.facessss@gmail.com>
2025-03-18 14:07:42 +0100

    OPF-101 updated astro and installed vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,15 @@ import tailwind from '@astrojs/tailwind'
 import sentry from '@sentry/astro';
 import spotlightjs from '@spotlightjs/astro';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+
   integrations: [react(), tailwind({
     applyBaseStyles: false,
   }), sentry(), spotlightjs()],
+
+  adapter: vercel(),
 })
