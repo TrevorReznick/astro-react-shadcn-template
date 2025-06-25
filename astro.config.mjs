e3a71e5d99c6186ebb5c466511cf48bca5e2bca8
TrevorReznick <electrics.sheeps@gmail.com>
2025-06-25 09:35:48 +0200

    OPF-101 fixing local

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,13 @@ import react from '@astrojs/react'
 
 import tailwind from '@astrojs/tailwind'
 
-import sentry from '@sentry/astro';
-import spotlightjs from '@spotlightjs/astro';
-
-import vercel from '@astrojs/vercel';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-
-  integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  }), sentry(), spotlightjs()],
-
-  adapter: vercel(),
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 })
